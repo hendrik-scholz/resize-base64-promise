47807783c113c082432b9106d442bd667332d705
hendrik-scholz <scholz-hendrik@web.de>
2019-09-25 21:19:19 +0200

    added linter

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,9 +4,16 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.1] - 2019-09-25
+### Added
+- added linter
+### Changed
+- applied lint rules to resize logic
+
 ## [1.0.0] - 2019-09-24
 ### Added
 - added resize functions with promise
 
-[Unreleased]: https://github.com/hendrik-scholz/resize-base64-promise/compare/1.0.0...HEAD
+[Unreleased]: https://github.com/hendrik-scholz/resize-base64-promise/compare/1.0.1...HEAD
+[1.0.1]: https://github.com/hendrik-scholz/resize-base64-promise/compare/1.0.0...1.0.1
 [1.0.0]: https://github.com/hendrik-scholz/resize-base64-promise/releases/tag/1.0.0
--- index.js
+++ index.js
@@ -1,150 +1,144 @@
-if (!String.prototype.startsWith) {
-  String.prototype.startsWith = function(search, pos) {
-	  return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
-  };
-}
-
 const DEFAULT_RATIO = 1;
 
 function validateInput(base64String, maxWidth, maxHeight) {
-	let validationResult = {
-		isValid: false,
-		errorMessage: 'An error occurred.'
-	};
+  const validationResult = {
+    isValid: false,
+    errorMessage: 'An error occurred.'
+  };
 
-	if(!base64String) {
-		validationResult.errorMessage = 'The input parameter base64String is ' + base64String + '.';
-	} else if(typeof(base64String) != 'string') {
-		validationResult.errorMessage = 'The input parameter base64String is not of type \'string\'.';
-	} else if(!base64String.startsWith('data:image')) {
-		validationResult.errorMessage = 'The input parameter base64String does not start with \'data:image\'.';
-	} else if(!maxWidth) {
-		validationResult.errorMessage = 'The input parameter maxWidth is ' + maxWidth + '.';
-	} else if(typeof(maxWidth) != 'number') {
-		validationResult.errorMessage = 'The input parameter maxWidth is not of type \'number\'.';
-	} else if(maxWidth < 2) {
-		validationResult.errorMessage = 'The input parameter maxWidth must be at least 2 pixel.';
-	} else if(!maxHeight) {
-		validationResult.errorMessage = 'The input parameter maxHeight is ' + maxHeight + '.';
-	} else if(typeof(maxHeight) != 'number') {
-		validationResult.errorMessage = 'The input parameter maxHeight is not of type \'number\'.';
-	} else if(maxHeight < 2) {
-		validationResult.errorMessage = 'The input parameter maxHeight must be at least 2 pixel.';
-	} else {
-		validationResult.isValid = true;
-		validationResult.errorMessage = null;
-	}
+  if (!base64String) {
+    validationResult.errorMessage = `The input parameter base64String is ${base64String}.`;
+  } else if (typeof (base64String) !== 'string') {
+    validationResult.errorMessage = 'The input parameter base64String is not of type \'string\'.';
+  } else if (!base64String.startsWith('data:image')) {
+    validationResult.errorMessage = 'The input parameter base64String does not start with \'data:image\'.';
+  } else if (!maxWidth) {
+    validationResult.errorMessage = `The input parameter maxWidth is ${maxWidth}.`;
+  } else if (typeof (maxWidth) !== 'number') {
+    validationResult.errorMessage = 'The input parameter maxWidth is not of type \'number\'.';
+  } else if (maxWidth < 2) {
+    validationResult.errorMessage = 'The input parameter maxWidth must be at least 2 pixel.';
+  } else if (!maxHeight) {
+    validationResult.errorMessage = `The input parameter maxHeight is ${maxHeight}.`;
+  } else if (typeof (maxHeight) !== 'number') {
+    validationResult.errorMessage = 'The input parameter maxHeight is not of type \'number\'.';
+  } else if (maxHeight < 2) {
+    validationResult.errorMessage = 'The input parameter maxHeight must be at least 2 pixel.';
+  } else {
+    validationResult.isValid = true;
+    validationResult.errorMessage = null;
+  }
 
-	return validationResult;
+  return validationResult;
 }
 
 function maxWidthRatioFunction(imageWidth, imageHeight, targetWidth, targetHeight) {
-	let ratio = DEFAULT_RATIO;
+  let ratio = DEFAULT_RATIO;
 
-	if(imageWidth > targetWidth) {
-		ratio = targetWidth / imageWidth;
-	}
+  if (imageWidth > targetWidth) {
+    ratio = targetWidth / imageWidth;
+  }
 
-	return {
-		width: ratio,
-		height: ratio
-	};
+  return {
+    width: ratio,
+    height: ratio
+  };
 }
 
 function maxHeightRatioFunction(imageWidth, imageHeight, targetWidth, targetHeight) {
-	let ratio = DEFAULT_RATIO;
+  let ratio = DEFAULT_RATIO;
 
-	if(imageHeight > targetHeight) {
-		ratio = targetHeight / imageHeight;
-	}
+  if (imageHeight > targetHeight) {
+    ratio = targetHeight / imageHeight;
+  }
 
-	return {
-		width: ratio,
-		height: ratio
-	};
+  return {
+    width: ratio,
+    height: ratio
+  };
 }
 
 function maxWidthMaxHeightRatioFunction(imageWidth, imageHeight, targetWidth, targetHeight) {
-	let widthRatio = targetWidth / imageWidth;
-	let heightRatio = targetHeight / imageHeight;
+  const widthRatio = targetWidth / imageWidth;
+  const heightRatio = targetHeight / imageHeight;
 
-	return {
-		width: widthRatio,
-		height: heightRatio
-	};
-}
-
-function resizeBase64ForMaxWidth(base64String, maxWidth, maxHeight) {
-	let validationResult = validateInput(base64String, maxWidth, maxHeight);
-
-	if(validationResult.isValid === true) {
-		return resizeBase64(base64String, maxWidth, maxHeight, maxWidthRatioFunction);
-	} else {
-		return new Promise((resolve, reject) => {
-			reject(validationResult.errorMessage);
-		});
-	}
-}
-
-function resizeBase64ForMaxHeight(base64String, maxWidth, maxHeight) {
-	let validationResult = validateInput(base64String, maxWidth, maxHeight);
-
-	if(validationResult.isValid === true) {
-		return resizeBase64(base64String, maxWidth, maxHeight, maxHeightRatioFunction);
-	} else {
-		return new Promise((resolve, reject) => {
-			reject(validationResult.errorMessage);
-		});
-	}
-}
-
-function resizeBase64ForMaxWidthAndMaxHeight(base64String, maxWidth, maxHeight) {
-	let validationResult = validateInput(base64String, maxWidth, maxHeight);
-
-	if(validationResult.isValid === true) {
-		return resizeBase64(base64String, maxWidth, maxHeight, maxWidthMaxHeightRatioFunction);
-	} else {
-		return new Promise((resolve, reject) => {
-			reject(validationResult.errorMessage);
-		});
-	}
+  return {
+    width: widthRatio,
+    height: heightRatio
+  };
 }
 
 function resizeBase64(base64String, maxWidth, maxHeight, ratioFunction) {
-	return new Promise((resolve, reject) => {
-		// Create and initialize two canvas
-		let canvas = document.createElement("canvas");
-		let ctx = canvas.getContext("2d");
-		let canvasCopy = document.createElement("canvas");
-		let copyContext = canvasCopy.getContext("2d");
+  return new Promise((resolve, reject) => {
+    // Create and initialize two canvas
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    const canvasCopy = document.createElement('canvas');
+    const copyContext = canvasCopy.getContext('2d');
 
-		// Create original image
-		let img = new Image();
-		img.src = base64String;
+    // Create original image
+    const img = new Image();
+    img.src = base64String;
 
-		img.onload = function() {
-			let ratioResult = ratioFunction(img.width, img.height, maxWidth, maxHeight);
-			let widthRatio = ratioResult.width;
-			let heightRatio = ratioResult.height;
+    img.onload = function onImageLoad() {
+      const ratioResult = ratioFunction(img.width, img.height, maxWidth, maxHeight);
+      const widthRatio = ratioResult.width;
+      const heightRatio = ratioResult.height;
 
-			// Draw original image in second canvas
-			canvasCopy.width = img.width;
-			canvasCopy.height = img.height;
-			copyContext.drawImage(img, 0, 0);
+      // Draw original image in second canvas
+      canvasCopy.width = img.width;
+      canvasCopy.height = img.height;
+      copyContext.drawImage(img, 0, 0);
 
-			// Copy and resize second canvas to first canvas
-			canvas.width = img.width * widthRatio;
-			canvas.height = img.height * heightRatio;
-			ctx.drawImage(canvasCopy, 0, 0, canvasCopy.width, canvasCopy.height, 0, 0, canvas.width, canvas.height);
+      // Copy and resize second canvas to first canvas
+      canvas.width = img.width * widthRatio;
+      canvas.height = img.height * heightRatio;
+      ctx.drawImage(canvasCopy, 0, 0, canvasCopy.width, canvasCopy.height, 0, 0, canvas.width, canvas.height);
 
-			resolve(canvas.toDataURL());
-		};
+      resolve(canvas.toDataURL());
+    };
 
-		img.onerror = function() {
-			reject('Error while loading image.');
-		};
-	});
-};
+    img.onerror = function onImageError() {
+      reject(new Error('Error while loading image.'));
+    };
+  });
+}
+
+function resizeBase64ForMaxWidth(base64String, maxWidth, maxHeight) {
+  const validationResult = validateInput(base64String, maxWidth, maxHeight);
+
+  if (validationResult.isValid === true) {
+    return resizeBase64(base64String, maxWidth, maxHeight, maxWidthRatioFunction);
+  } else {
+    return new Promise((resolve, reject) => {
+      reject(validationResult.errorMessage);
+    });
+  }
+}
+
+function resizeBase64ForMaxHeight(base64String, maxWidth, maxHeight) {
+  const validationResult = validateInput(base64String, maxWidth, maxHeight);
+
+  if (validationResult.isValid === true) {
+    return resizeBase64(base64String, maxWidth, maxHeight, maxHeightRatioFunction);
+  } else {
+    return new Promise((resolve, reject) => {
+        reject(validationResult.errorMessage);
+    });
+  }
+}
+
+function resizeBase64ForMaxWidthAndMaxHeight(base64String, maxWidth, maxHeight) {
+  const validationResult = validateInput(base64String, maxWidth, maxHeight);
+
+  if (validationResult.isValid === true) {
+    return resizeBase64(base64String, maxWidth, maxHeight, maxWidthMaxHeightRatioFunction);
+  } else {
+    return new Promise((resolve, reject) => {
+        reject(validationResult.errorMessage);
+    });
+  }
+}
 
 export { resizeBase64ForMaxWidth };
 export { resizeBase64ForMaxHeight };
